80c86ccda494cb8eac00400ee66dcfef1359bfd8
Lucas Kellem <lucaskellem@mines.edu>
2022-09-21 15:53:40 -0600

    Test

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,43 @@
 
 public class ScoreTrakker {
-
+	private ArrayList<Student> studentList;
+	
+	public ScoreTrakker() {
+		this.studentList = new ArrayList<Student>();
+	}
+	
+	void loadDataFile(String fileName) {
+		/*
+		 * Has a method named loadDataFile() that takes the file name (a String) 
+		 * as a parameter. This method reads student data from a file (format described below) 
+		 * and stores student objects in the ArrayList.
+		 */
+	}
+	
+	void printInOrder() {
+		/*
+		 * Has a method named printInOrder() that will sort and print the ArrayList.
+		 * You don't need to write a sort method (google to figure this out),
+		 * Try to use an iterated for loop (for each), rather than a counter-controlled loop 
+		 * (i.e., no for (int i=0; )
+		 */
+		
+	}
+	
+	void processFiles() {
+		/*
+		 * For now, this method will:
+		 * Call the loadDataFile() method passing in the name of a correctly formatted file ("scores.txt").
+		 * Call the printInOrder() method.
+		 */
+	}
+	
+	public static void main(String[] args) {
+		/*
+		 * Has a main() method that simply creates a new object of type ScoreTrakker
+		 * and calls processFiles().
+		 */
+		ScoreTrakker scores = new ScoreTrakker(); 
+		scores.processFiles();
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,31 @@
 
-public class Student {
+public class Student implements Comparable<Student>{
+	private String name;
+	private int score;
+	
+	public Student(String inName, int inScore) {
+		this.name = inName;
+		this.score = inScore;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " " + score;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		//if we want to compare scores:
+//		if(this.score < o.score) {
+//			return -1;
+//		}else if(this.score > o.score) {
+//			return 1;
+//		}else {
+//			return 0;
+//		}
+		
+		//if we want to compare names:
+		return this.name.compareTo(o.name);
+	}
 
 }
